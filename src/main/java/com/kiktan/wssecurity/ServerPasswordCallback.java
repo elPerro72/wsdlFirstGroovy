7c8443d86d80bfe7bc8e5d1d0720c5bd5507e289
elPerro <>
2018-11-14 21:59:38 +0100

    first commit

---
+++ src/main/java/com/kiktan/wssecurity/ServerPasswordCallback.java
@@ -0,0 +1,24 @@
+package com.kiktan.wssecurity;
+
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+public class ServerPasswordCallback implements CallbackHandler {
+    
+    public ServerPasswordCallback() {
+    }
+
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
+        if ("jon".equals(pc.getIdentifier())) {
+            pc.setPassword("secret");
+        } else {
+            throw new IOException("Username incorrect!");   
+        }
+    }
+}
