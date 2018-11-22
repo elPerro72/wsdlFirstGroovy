bd474e6b8cd085c26667dd13fa5f347151b95235
elPerro72 <raymonddehond@gmail.com>
2018-11-22 21:48:58 +0100

    test xmlSlurper

--- src/main/groovy/com/kiktan/test/testGroovy.groovy
+++ src/main/groovy/com/kiktan/test/testGroovy.groovy
@@ -3,6 +3,11 @@ package com.kiktan.test;
 public class testGroovy {
 
     public static String getHeaders(String custom) {
+
+
+        def inputSOATest = new XmlSlurper().parseText(custom)
+        def ff =  inputSOATest.test
+
         return custom
     }
 }
